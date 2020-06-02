4ad54b08adf8e736dcfd893e5b884b11a60d5ca7
Stanislav Burlak <ikonostasik@gmail.com>
2020-06-02 23:09:31 +0300

    chunk function realization

---
+++ Array/chunk.js
@@ -0,0 +1,27 @@
+/**
+ * Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+ * @param  {Array}  [array]  An array of elements that should be split to chunks
+ * @param  {number} [size=1] Amount of chunks that should be returned
+ * @return {Array}           Array of arrays
+ */
+
+function chunk(array, size = 1) {
+  if (!array.length || typeof size !== 'number') {
+    throw 'Wrong arguments provided';
+  }
+
+  if (size >= array.length) {
+    return [array];
+  }
+
+  const result = [];
+  let temp = [];
+
+  while (array.length) {
+    temp = array.splice(size);
+    result.push(array);
+    array = temp;
+  }
+
+  return result;
+}
