fb11f218995d377064456be58219b666abc9fe25
Stanislav Burlak <ikonostasik@gmail.com>
2020-06-04 00:28:07 +0300

    difference funciton

---
+++ Array/difference.js
@@ -0,0 +1,21 @@
+/**
+ * Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons.
+ * The order and references of result values are determined by the first array.
+ * @param {Array} [array]  Base array
+ * @param {Array} [values] Array of values to be removed from base array
+ * @return {Array}         Filtered array
+ */
+
+function difference(array,values) {
+  if (!Array.isArray(array)) {
+    return  []
+  }
+
+  if (!Array.isArray(values)) {
+    return array;
+  }
+
+  return array.filter( el => !values.includes(el));
+}
+
+
